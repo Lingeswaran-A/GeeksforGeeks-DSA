d5c9458c29cf7255de95ce498344638435427c36
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-01-06 19:02:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            ArrayList<Integer> array = new ArrayList<>();
+
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int target = Integer.parseInt(br.readLine());
+
+            Solution ob = new Solution();
+            List<Integer> res = ob.sumClosest(arr, target);
+            if (res.isEmpty()) {
+                System.out.print("[]");
+            } else {
+                for (Integer num : res) {
+                    System.out.print(num + " ");
+                }
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public List<Integer> sumClosest(int[] arr, int target) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        int diff=Integer.MAX_VALUE;
+        List<Integer> res=new ArrayList<>();
+        int left=0,right=n-1;
+        while(left<right){
+            int cur_sum=arr[left]+arr[right];
+            if(Math.abs(target-cur_sum)<diff){
+                diff=Math.abs(target-cur_sum);
+                res=Arrays.asList(arr[left],arr[right]);
+            }
+            if(cur_sum<target){
+                left++;
+            }
+            else if(cur_sum>target){
+                right--;
+            }
+            else{
+                return res;
+            }
+        }
+        return res;
+    }
+}
